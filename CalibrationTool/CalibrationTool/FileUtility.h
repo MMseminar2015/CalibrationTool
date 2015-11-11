ab252c75db59661a66c115b5d3faa28f05f76953
0kazuya <ohara.kazuya@ist.osaka-u.ac.jp>
2015-11-11 18:25:49 +0900

    ファイル追加

--- CalibrationTool/CalibrationTool/FileUtility.h
+++ CalibrationTool/CalibrationTool/FileUtility.h
@@ -10,8 +10,8 @@ public:
 	/// </summary>
 	static std::vector<std::string> GetFilesFromDirectory(std::string dirpath, const std::string& filter);
 
-	//static std::string AddSuffix(std::string file, std::string suffix);
-	//static std::string GetFileName(std::string filepath);
-	//static std::string GetDirPath(std::string filepath);
-	//static std::string Replace(std::string String1, std::string String2, std::string String3);
+	static std::string AddSuffix(std::string file, std::string suffix);
+	static std::string GetFileName(std::string filepath);
+	static std::string GetDirPath(std::string filepath);
+	static std::string Replace(std::string String1, std::string String2, std::string String3);
 };
